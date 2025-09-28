4b1699d489fe7aada37c26d6a82fbf5019a342ea
opener <opener@ubuntu.dlink>
2025-09-28 09:10:48 +0000

    init

---
+++ App.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import {Pressable, StyleSheet, Text, View, Image} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import Lato from "./pages/lato";
+
+const staticImage = require("./assets/home.png");
+
+const App = () => {
+  
+  const [isLatoActive, activateLato] = useState(false);
+
+  function renderHome() {
+    if(!isLatoActive) {
+      return (
+        <Pressable
+          onPress={() => activateLato(true) }
+          style={styles.homeImageWrapper}>
+          {({pressed}) => {
+            if(!isLatoActive) {
+              return (
+                <Image
+                  source={staticImage}
+                  style={styles.homeImage}
+                />
+              )
+            }
+           }}
+        </Pressable>  
+      )
+    }
+  }
+
+  function renderLato() {
+    if(isLatoActive) {
+      return (
+        <Lato/>
+      )
+    }
+  }
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {renderHome()}
+        {renderLato()}
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 427,
+    height: 896,
+  },
+  homeImageWrapper: {
+    borderRadius: 8,
+    padding: 6,
+    height: 100
+  },
+  homeImage: {
+    flex: 1,
+    resizeMode: "cover",
+    width: 427,
+    height: 896,
+    alignItems: "center",
+  },
+  });
+
+export default App;
---
+++ pages/lato.js
@@ -0,0 +1,7 @@
+import { Router, Route, Link } from "../react-router";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+
+const Lato = () => <Text>Lato</Text>;
+
+export default Lato;
